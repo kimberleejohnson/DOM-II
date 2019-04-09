8c4d5d559ef5ddf7f790596bef18e66d5606eee4
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-09 15:11:59 -0700

    Added different event listeners

--- js/index.js
+++ js/index.js
@@ -1,48 +1,97 @@
 // Adding event listeners to the site 
 
+// Body 
+
+const body = document.querySelector('body'); 
+body.addEventListener('keypress', function(eventObject) {
+    body.style.backgroundColor = 'red';
+});
+
 // Nav logo 
 const navLogo = document.querySelector('.logo-heading'); 
 // console.log(navLogo);
+navLogo.addEventListener('mouseover', function(eventObject) {
+    console.log('We moused over:', eventObject.clientX);
+});
 
 // Nav items
-const navItems = document.querySelector('.nav'); 
-// console.log(navItems);
+const navItems1 = document.querySelector('.nav-link1'); 
+navItems1.addEventListener('click', function(eventObject) {
+       console.log(`We clicked on ${eventObject.clientX}`);                 
+ });
+
+const navItems2 = document.querySelector('.nav-link2'); 
+navItems2.addEventListener('click', function(eventObject) {
+       console.log(`We clicked on ${eventObject.clientX}`);                 
+ });
+
+const navItems3 = document.querySelector('.nav-link3'); 
+navItems3.addEventListener('click', function(eventObject) {
+       console.log(`We clicked on ${eventObject.clientX}`);                 
+ });
+
+const navItems4 = document.querySelector('.nav-link4'); 
+navItems4.addEventListener('click', function(eventObject) {
+       console.log(`We clicked on ${eventObject.clientX}`);                 
+ });
 
 // Intro bus image 
 const introImg = document.querySelector('.intro img'); 
-// console.log(introImg);
+introImg.addEventListener('load', function(eventObject) {
+    console.log("Bus loaded!");
+});    
 
 // Welcome to Fun Bus! intro header 
 const introHeader = document.querySelector('.intro h2'); 
-// console.log(introHeader);
+introHeader.addEventListener('copy', function(eventObject) {
+    console.log("Woah, you copied me!");
+});  
 
 // Let's go! header
 const letsGo = document.querySelector('.content-section h2'); 
-// console.log(letsGo);
+letsGo.addEventListener('mouseenter', function(eventObject) {
+    console.log("Thanks for selecting me!");
+}); 
 
 // Let's go! Img 
 const letsGoImg = document.querySelector('.content-section img'); 
-// console.log(letsGoImg);
+letsGoImg.addEventListener('mouseleave', function(eventObject) {
+    console.log("Thanks for leaving your mouse");
+});
 
 // Adventure awaits! Header
 const adventureAwaits = document.querySelector('.inverse-content h2'); 
-// console.log(adventureAwaits);
+adventureAwaits.addEventListener('mousemove', function(eventObject) {
+    console.log("Thanks for moving the mouse over me");
+});
 
 // Adventure awaits! Img 
 const adventureImg = document.querySelector('.inverse-content img'); 
+adventureImg.addEventListener('mouseleave', function(eventObject) {
+    console.log("Thanks for leaving your mouse");
+});
 // console.log(adventureImg);
-
-// Adventure awaits image 
-
-// Pick your destination! 
-const pickDestination = document.querySelector('.content-destination h2'); 
-// console.log(pickDestination);
 
 // Destination image 
 const destinationImg = document.querySelector('.content-destination img'); 
-// console.log(destinationImg);
+destinationImg.addEventListener('mouseup', function(eventObject) {
+    console.log("Thanks for the mouseup!");
+});
 
 // Buttons 
-const destinationBtn = document.querySelector('.btn'); 
-// console.log(destinationBtn);
+const destinationBtn1 = document.querySelector('.btn1'); 
+destinationBtn1.addEventListener('dblclick', function(eventObject) {
+    console.log("Thanks for the dblclick!");
+});
+
+const destinationBtn2 = document.querySelector('.btn2'); 
+destinationBtn2.addEventListener('dblclick', function(eventObject) {
+    console.log("Thanks for the dblclick!");
+});
+
+const destinationBtn3 = document.querySelector('.btn3'); 
+destinationBtn3.addEventListener('dblclick', function(eventObject) {
+    console.log("Thanks for the dblclick!");
+});
+
 
