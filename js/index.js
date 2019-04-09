6e510443cf898c265ed770bbae54d835f4d25cab
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-09 13:25:19 -0700

    Selected items to add event listeners to

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
-// Your code goes here
+// Adding event listeners to the site 
+
+// Nav logo 
+const navLogo = document.querySelector('.logo-heading'); 
+// console.log(navLogo);
+
+// Nav items
+const navItems = document.querySelector('.nav'); 
+// console.log(navItems);
+
+// Intro bus image 
+const introImg = document.querySelector('.intro img'); 
+// console.log(introImg);
+
+// Welcome to Fun Bus! intro header 
+const introHeader = document.querySelector('.intro h2'); 
+// console.log(introHeader);
+
+// Let's go! header
+const letsGo = document.querySelector('.content-section h2'); 
+// console.log(letsGo);
+
+// Let's go! Img 
+const letsGoImg = document.querySelector('.content-section img'); 
+// console.log(letsGoImg);
+
+// Adventure awaits! Header
+const adventureAwaits = document.querySelector('.inverse-content h2'); 
+// console.log(adventureAwaits);
+
+// Adventure awaits! Img 
+const adventureImg = document.querySelector('.inverse-content img'); 
+// console.log(adventureImg);
+
+// Adventure awaits image 
+
+// Pick your destination! 
+const pickDestination = document.querySelector('.content-destination h2'); 
+// console.log(pickDestination);
+
+// Destination image 
+const destinationImg = document.querySelector('.content-destination img'); 
+// console.log(destinationImg);
+
+// Buttons 
+const destinationBtn = document.querySelector('.btn'); 
+// console.log(destinationBtn);
+
